c18ebf9112979cdb858b9485d2a53011ad4e2e07
Danny Kim <dudkny@gmail.com>
2020-05-04 18:25:41 -0500

    changed server port

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,23 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 
-const app = express()
+const app = express();
 
-app.use(express.static('src/client'))
+app.use(express.static('src/client'));
 
-console.log(__dirname)
+console.log('* Current directory:', __dirname);
 
 app.get('/', function (req, res) {
-    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
-})
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' });
+});
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+const port = 8081;
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+    res.send(mockAPIResponse);
+});
