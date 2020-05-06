128c344b0405f309a2a10d0c1645ed012d8b884a
Danny Kim <dudkny@gmail.com>
2020-05-06 10:48:58 -0500

    implemented webpack config for dev & prod

---
+++ webpack.dev.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/client/index.js",
+    module: {
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+}
