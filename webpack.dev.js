85dcd979d131534e5ce0d39f3d307666d4cf58f6
Danny Kim <dudkny@gmail.com>
2020-05-06 17:20:04 -0500

    implemented Webpack Bundle Analyzer

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
@@ -31,6 +32,7 @@ module.exports = {
             template: './src/client/views/index.html',
             filename: './index.html'
         }),
+        // TODO: after implementation of CleanWebpackPlugin, there is error message about source map.
         new CleanWebpackPlugin({
             // Simulate the removal of files
             dry: true,
@@ -39,6 +41,10 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
+        }),
+        new BundleAnalyzerPlugin({
+            generateStatsFile: true,
+            openAnalyzer: true
         })
     ]
 }
